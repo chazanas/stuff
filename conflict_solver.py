db7dc71957e455fc77f2c5e8bc47fe78441c5824
chazanas <chazanaspln@gmail.com>
2018-06-24 18:57:46 -0300

    conflict started

--- conflict_solver.py
+++ conflict_solver.py
@@ -1,1 +1,15 @@
 import re
+
+## read py file
+
+def conflict_solver(path, head):
+	'''(str, bool) -> none
+	gets a conflicted path and undo its conflicts, choosing the first option if
+	head is True, or the second when the oposite.
+	'''
+
+	code = open(path, 'r').read()
+
+	regex = '>>+'
+
+
---
+++ dammed.py
@@ -0,0 +1,24 @@
+
+
+print('not conflict')
+print('not not')
+print('maybe')
+
+
+
+
+<<<<<<<<<<<<<<<<<<<<<< HEAD
+
+for i in range(1000):
+	print('hi')
+
+================
+
+for j in range(1000):
+	print('bye')
+
+
+>>>>>>>>>>>>>>>>>>> 23243438473462abc327367ef87182631723abcd1
+
+
+print('yeah yeah')
